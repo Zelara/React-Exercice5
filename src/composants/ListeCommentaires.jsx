ddd5d98df0a96ce7264778fcdb47bc53b1bdb596
Zelara <119063805+Zelara@users.noreply.github.com>
2024-05-03 14:01:18 +0000

    je dois quitter

--- src/composants/ListeCommentaires.jsx
+++ src/composants/ListeCommentaires.jsx
@@ -1,37 +1,37 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { obtenir } from "../code/commentaire-modele";
 
 export default function ListeCommentaires() {
   const [commentaires, setCommentaires] = useState([]);
-  const [totalVotesPositifs, setTotalVotesPositifs] = useState(0);
-  const [totalVotesNegatifs, setTotalVotesNegatifs] = useState(0);
 
   async function obtenirCommentaires() {
     const idBandeTest = "1wVeiCGQPyPfPM7ys0c4";
     let comms = await obtenir(idBandeTest);
     setCommentaires(comms);
-
-    // Calcul du total des votes positifs et négatifs
-    const votes = comms.map((comm) => comm.votes);
-    const valeursVotes = Object.values(votes);
-    const totalPositifs = valeursVotes.reduce((acc, val) => (val === 1 ? acc + 1 : acc), 0);
-    const totalNegatifs = valeursVotes.reduce((acc, val) => (val === -1 ? acc + 1 : acc), 0);
-    setTotalVotesPositifs(totalPositifs);
-    setTotalVotesNegatifs(totalNegatifs);
   }
 
   useEffect(() => {
     obtenirCommentaires();
   }, []);
 
+  // Calcul du total des votes positifs et négatifs
+  const votesArray = Object.values(commentaires.reduce((acc, commentaire) => {
+    const votes = commentaire.votes;
+    acc[votes] = (acc[votes] || 0) + 1;
+    return acc;
+  }, {}));
+
+  const totalVotesPositifs = votesArray.reduce((acc, value, index) => {
+    return index > 0 ? acc + value : acc;
+  }, 0);
+
+  const totalVotesNegatifs = votesArray.reduce((acc, value, index) => {
+    return index < 0 ? acc + value : acc;
+  }, 0);
+
   return (
     <div className="ListeCommentaires">
       <h2>Statistiques</h2>
-      <div>
-        <p>Total des votes positifs : {totalVotesPositifs}</p>
-        <p>Total des votes négatifs : {totalVotesNegatifs}</p>
-      </div>
-      <h2>Commentaires associés à la bande quotidienne</h2>
       <ul>
         {commentaires.map((commentaire) => (
           <li key={commentaire.id}>
@@ -44,6 +44,8 @@ export default function ListeCommentaires() {
           </li>
         ))}
       </ul>
+      <h3>Total des votes positifs : {totalVotesPositifs}</h3>
+      <h3>Total des votes négatifs : {totalVotesNegatifs}</h3>
     </div>
   );
 }
